33eacc1f337423976148218b09a024e0cfae1a39
Tanja Gruber <tgruber.mmt-b2021@fh-salzburg.ac.at>
2022-11-16 22:50:36 +0100

    add composer files, modify deploy.php

--- deploy.php
+++ deploy.php
@@ -2,17 +2,20 @@
 
 namespace Deployer;
 
-require 'recipe/composer.php';
+require 'recipe/common.php';
 
 // Config
 
 set('repository', 'https://github.com/Jasonk3nny/wpaaron_tanja_jason');
 
 
-
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
+// set('git_tty', true);
 
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
 
 
 // Shared files/dirs between deploys
@@ -24,38 +27,43 @@ set('shared_dirs', ['public/wp-content/uploads']);
 // Writable dirs by web server
 // set('writable_mode', 'chown');
 // set('writable_dirs', ['public/wp-content/uploads']);
-set('allow_anonymous_stats', false);
-
+// set('allow_anonymous_stats', false);
 
 
 // Hosts
 host('vm-alabaster.multimediatechnology.at')
-    ->user('admin')
-    ->port(5412)
+    ->set('remote_user', 'admin')
+    ->set('port','5412')
     ->set('deploy_path', '/home/admin/aaron');
 
 
 
 
 // Composer
-set('composer_action', false);
+// set('composer_action', false);
 
 
 
 
 // Tasks
+// desc('Deploy your project');
+// task('deploy', [
+//     'deploy:info',
+//     'deploy:prepare',
+//     'deploy:lock',
+//     'deploy:release',
+//     'deploy:update_code',
+//     'deploy:shared',
+//     'deploy:writable',
+//     'deploy:clear_paths',
+//     'deploy:symlink',
+//     'deploy:unlock',
+// ]);
+
 desc('Deploy your project');
 task('deploy', [
-    'deploy:info',
     'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
+    'deploy:publish'
 ]);
 
 
