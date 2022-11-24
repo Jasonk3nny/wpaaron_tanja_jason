b6ff279b2146ee6a43e0b680b7904a7d93757bf4
Tanja Gruber <tgruber.mmt-b2021@fh-salzburg.ac.at>
2022-11-24 16:07:57 +0100

    tweak build script so that entire php folder is built

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-
-# npx sass ./src/sass/style.scss ./public/wp-content/aaron/style.css
-# cp ./src/index.html ./public/
-# cp ./src/favicons/*.* ./public/
-# cp -R ./src/images ./public/
-# cp -R ./src/fonts ./public/fonts
-# npx esbuild ./src/js/main.js --bundle --outfile=./public/main.js  --minify
-
-cp ./src/php/index.php ./public/wp-content/themes/aaron/index.php
+cp ./src/php/*.* ./public/wp-content/themes/aaron/
 cp -R ./src/images ./public/wp-content/themes/aaron
 cp -R ./src/fonts ./public/wp-content/themes/aaron
 cp -R ./src/favicons ./public/wp-content/themes/aaron
