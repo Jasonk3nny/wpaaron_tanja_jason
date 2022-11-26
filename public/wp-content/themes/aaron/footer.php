9be85b0d50dcf9c1718efd328d35af8d53da9542
Jason Christopher Kenny <jkenny.mmt-b2021@fh-salzburg.ac.at>
2022-11-27 00:08:09 +0100

    added img in header

--- public/wp-content/themes/aaron/footer.php
+++ public/wp-content/themes/aaron/footer.php
@@ -1,6 +1,6 @@
 <?php wp_footer(); ?>
 <footer>
-    <h4>Thank you for your interest in Art!</h4>
-    <p>© 2004 – 2023 Aaron G. Miller</p>
-    <a href="#">Imprint & Privacy</a>
+	<h4>Thank you for your interest in Art!</h4>
+	<p>© 2004 – 2023 Aaron G. Miller</p>
+	<a href="#">Imprint & Privacy</a>
 </footer>
