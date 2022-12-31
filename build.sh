72d0b92e3c947fca1e2c1ef81ec8eb283ca2b5dc
Jason Christopher Kenny <jkenny.mmt-b2021@fh-salzburg.ac.at>
2022-12-31 01:38:34 +0100

    added header img

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ cp ./src/php/*.* ./public/wp-content/themes/aaron/
 cp -R ./src/images ./public/wp-content/themes/aaron
 cp -R ./src/fonts ./public/wp-content/themes/aaron
 cp -R ./src/favicons ./public/wp-content/themes/aaron
-npx sass ./src/sass/style.scss ./public/wp-content/themes/aaron/style.css
+npx sass ./src/sass/style.scss ./public/wp-content/themes/aaron/style.css.map
 npx esbuild ./src/js/main.js --bundle --outfile=./public/wp-content/themes/aaron/main.js  --minify
 
